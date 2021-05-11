24d58166930dbb134366556d66a482eadae60c13
infin <infin@DESKTOP-CGOTLLG>
2021-05-10 23:51:42 -0500

    edit spelling

---
+++ project2visual/visual bar/app.js
@@ -0,0 +1,15 @@
+
+// function to create create bar chart
+
+function _id( sample){ 
+    d3.json ("country.json").then((data) => {
+        var _id = data._id;
+        var filter_id_array = _id.filter(sampleobject=>sampleobject.id == sample)
+        var  filter_id_result = filter_id_array[0];
+        var Panel = d3.select("#_id");
+        Panel.html("");
+        Object.entries(filter_id_result).forEach(([key,value]) => {
+        Panel.append("h6").text(`${key}:${value}`);
+        });
+    });
+}
